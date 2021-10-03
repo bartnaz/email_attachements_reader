e394a1ce06bd0d4f82131307a6031efeba324cb2
bartnaz <bartosz.naz@gmail.com>
2021-10-03 11:22:41 +0200

    feat(gui) gui addon to reader

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
 from .service_mixin import AuthMixin, ServiceMixin
 from .attachements_reader import EmailReader
+from .core import GuiClass
 
-__all__ = ["AuthMixin", "ServiceMixin", "EmailReader"]
+__all__ = ["AuthMixin", "ServiceMixin", "EmailReader", "GuiClass"]
